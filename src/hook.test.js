ed8a5fd9426d23c247d6b641ffbce71b3edfd4bc
DrizzlingCattus <hit0473@gmail.com>
2019-10-26 17:45:20 +0900

    Feat: Hook test code

---
+++ src/hook.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import HookCounter from '../src/hook.js';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+class Temp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<HookCounter/>);
+    }
+}
+describe('<HookCounter/>', () => {
+    test('increases', () => {
+        const wrapper = mount(<HookCounter/>, { context: { foo: 10 }});
+        let plusButton = wrapper.findWhere(node => {
+            return node.type() === 'button' && node.text() === '+1'
+        });
+        plusButton.simulate('click');
+        plusButton.simulate('click');
+
+        console.log(wrapper.context('foo'));
+        console.log(wrapper.state());
+        const number = wrapper.find('h2');
+
+        expect(number.text()).toBe('2');
+    });
+});
+
