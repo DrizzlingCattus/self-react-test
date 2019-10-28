a04e6bc662c3ca91c3a89c3cf5f7cf65605ee752
DrizzlingCattus <hit0473@gmail.com>
2019-10-28 22:15:02 +0900

    Chore: push deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@ export DISPLAY=:0
 mkfifo -m=600 fifo
 # why ssh-agent is needed to eval
 # https://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#408675://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#4086756
-eval $(ssh-agent -s)
+eval $(ssh-agent)
 
 ask="./temp"
 echo "echo ''" > $ask
 chmod +x $ask
-
-echo "$SSH_SECRET" >fifo | SSH_ASKPASS=$ask ssh-add fifo
+SSH_ASKPASS=''
+echo "$SSH_SECRET" >fifo | ssh-add fifo
 scp -o "StrictHostKeyChecking no" -P $SSH_PORT -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
