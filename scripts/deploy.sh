c694c48901015e335844afca4d66dfab9d2da0b3
DrizzlingCattus <hit0473@gmail.com>
2019-10-28 23:17:06 +0900

    Chore: travis keygen test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,9 @@ export DISPLAY=:0
 mkfifo -m=600 fifo
 # why ssh-agent is needed to eval
 # https://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#408675://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#4086756
-eval $(ssh-agent)
+eval $(ssh-agent -s)
 
 SSH_ASKPASS=''
-#echo "$SSH_SECRET" >fifo | ssh-add fifo
-which ssh-add
-ssh-add <(echo "$SSH_SECRET")
+echo "$SSH_SECRET" >fifo | ssh-add fifo
+#ssh-add <(echo "$SSH_SECRET")
 scp -o "StrictHostKeyChecking no" -P $SSH_PORT -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
