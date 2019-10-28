5e318042ed667df4046561e57fc5ea3da898e5be
DrizzlingCattus <hit0473@gmail.com>
2019-10-28 21:41:53 +0900

    Chore: display 변수 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
+# Set no display, necessary for ssh to play nice with setsid and SSH_ASKPASS.
+export DISPLAY=:0
 
 mkfifo -m=600 fifo
 # why ssh-agent is needed to eval
 # https://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#408675://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#4086756
 eval $(ssh-agent -s)
 
+ask="./temp"
 echo "echo ''" > $ask
 chmod +x $ask
 
