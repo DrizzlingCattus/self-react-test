8e6982b5030cfd3fb20f6d12629c5502d0b79513
DrizzlingCattus <hit0473@gmail.com>
2019-10-28 21:32:50 +0900

    Chore: SSH_ASKPASS pass

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@ mkfifo -m=600 fifo
 # why ssh-agent is needed to eval
 # https://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#408675://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent/4086756#4086756
 eval $(ssh-agent -s)
-echo "$SSH_SECRET"
-echo "$SSH_SECRET" >fifo | ssh-add fifo
-scp -p $SSH_PORT -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
+
+echo "echo ''" > $ask
+chmod +x $ask
+
+echo "$SSH_SECRET" >fifo | SSH_ASKPASS=$ask ssh-add fifo
+scp -P $SSH_PORT -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
