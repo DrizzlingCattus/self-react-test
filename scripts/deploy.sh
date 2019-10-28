5f39e17d93f8ba755d17b6f675d692eaf7df52e0
DrizzlingCattus <hit0473@gmail.com>
2019-10-28 20:42:57 +0900

    Chore: named pipe를 이용한 ssh-key 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,4 @@
-scp -p $SSH_PORT -i <($SSH_SECRET) -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
+
+mkfifo -m=600 fifo
+echo $SSH_SECRET >fifo | ssh-add fifo
+scp -p $SSH_PORT -r build stenrine@boostcamp.stenrine.com:/home/stenrine/build
